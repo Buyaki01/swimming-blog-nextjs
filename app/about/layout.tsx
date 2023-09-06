0dcf285b59ce19cf7a48e10d46d3be19e5da87c9
Buyaki01 <rittahbuyaki@gmail.com>
2023-09-06 10:41:32 +0300

    Add AboutLayout

---
+++ app/about/layout.tsx
@@ -0,0 +1,14 @@
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <nav>About Navbar</nav>
+      <main>
+        {children}
+      </main>
+    </>
+  )
+}
