b95ae7406495194b99406b232b315b2911f93e0e
Buyaki01 <rittahbuyaki@gmail.com>
2023-09-07 06:13:27 +0300

    Add styles.modules.css in the about folder

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <h1>Hello World!</h1>
       <Link href="/about">Go to About Page</Link>
     </main>
