dda1a308e343dff3e6ac8a119aeadf43c3391a8b
Buyaki01 <rittahbuyaki@gmail.com>
2023-09-07 05:54:04 +0300

    Remove the About folder, Update app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World!</h1>
-      <Link href="/about">Go to About Page</Link>
+      <h1>Home Page</h1>
+      <p>
+        <Link href="/users">Users</Link>
+      </p>
     </main>
   )
 }
