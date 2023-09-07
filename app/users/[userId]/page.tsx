af4064fe995f6d9cd37b99f0eb6d4310733d1d77
Buyaki01 <rittahbuyaki@gmail.com>
2023-09-07 10:22:37 +0300

    Add userData, userPostsData which is fetching data from server, add Suspense

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,11 +1,27 @@
+import getUser from "@/lib/getUser"
+import getUserPosts from "@/lib/getUserPosts"
+import { Suspense } from "react"
+
 type Params = {
   params: {
     userId: string
   }
 }
 
-export default function UserPage({ params: { userId } }: Params) {// This component takes an object as a parameter, and it expects this object to have a params property that contains a userId property
+export default async function UserPage({ params: { userId } }: Params) {// This component takes an object as a parameter, and it expects this object to have a params property that contains a userId property
+  const userData: Promise<User> = getUser(userId)
+  const userPostsData: Promise<Post[]> = getUserPosts(userId)
+  
+  const [user, userPosts] = await Promise.all([userData, userPostsData])
+
   return (
-    <div></div>
+    <>
+      <h2>{user.name}</h2>
+      <br />
+      
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <UserPosts posts={userPosts} />
+      </Suspense>
+    </>
   )
 }
