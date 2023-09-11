be1835d670305c048164f277d7f0caea79b93af5
Buyaki01 <rittahbuyaki@gmail.com>
2023-09-11 12:23:43 +0300

    Add the default of cache to the fetch request

--- lib/getUserPosts.tsx
+++ lib/getUserPosts.tsx
@@ -1,5 +1,5 @@
 export default async function getUserPosts(userId: string) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, { cache: 'force-cache' })
 
   if (!res.ok) throw new Error('failed to fetch user')
 
