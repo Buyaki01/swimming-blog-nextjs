ee5278df2c5ce7cc11209071ba2ef99d012d65a2
Buyaki01 <rittahbuyaki@gmail.com>
2023-09-14 06:51:23 +0300

    Add content of the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World!</h1>
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I'm <span className="font-bold">Ritta</span>
+        </span>
+      </p>
     </main>
   )
 }
