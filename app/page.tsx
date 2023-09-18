7cb9512cb710b599166059421858c432fd35b4f6
Buyaki01 <rittahbuyaki@gmail.com>
2023-09-18 12:01:53 +0300

    Add Posts component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Posts from "./components/Posts"
+
 export default function Home() {
   return (
     <main className="px-6 mx-auto">
@@ -7,6 +9,8 @@ export default function Home() {
           I'm <span className="font-bold">Ritta</span>
         </span>
       </p>
+
+      <Posts />
     </main>
   )
 }
