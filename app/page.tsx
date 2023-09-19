5aa5ab5deac596cc9ccb93984bcdce0d259abcb8
Buyaki01 <rittahbuyaki@gmail.com>
2023-09-19 08:34:07 +0300

    Display content of dynamic page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
         Hello and Welcome 👋&nbsp;
         <span className="whitespace-nowrap">
-          I'm <span className="font-bold">Ritta</span>
+          I&apos;m <span className="font-bold">Ritta</span>
         </span>
       </p>
 
--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,4 +1,6 @@
-import { getSortedPostsData } from "@/lib/posts"
+import getFormattedDate from "@/lib/getFormattedDate"
+import { getPostData, getSortedPostsData } from "@/lib/posts"
+import Link from "next/link"
 import { notFound } from "next/navigation"
 
 export function generateMetadata({ params }: { params: { postId: string }}) {
@@ -26,7 +28,22 @@ export default async function Post({ params }: { params: { postId: string }}) {
     return notFound()
   }
 
+  const { title, date, contentHtml } = await getPostData(postId)
+
+  const pubDate = getFormattedDate(date)
+
   return (
-    <div></div>
+    <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+      <h1 className="text-3xl mt-4 mb-0">{title}</h1>
+      <p className="mt-0">
+        {pubDate}
+      </p>
+      <article>
+        <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+        <p>
+          <Link href="/"> Back to home</Link>
+        </p>
+      </article>
+    </main>
   )
 }
