5b9c898ab4de099106f88900b5d5072c2b6b56aa
Buyaki01 <rittahbuyaki@gmail.com>
2023-09-19 06:51:16 +0300

    Install remark, remark-html, Add page.tsx for post dynamic route, Add getPostData method in lib/posts

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,6 +1,23 @@
 import { getSortedPostsData } from "@/lib/posts"
 import { notFound } from "next/navigation"
 
+export function generateMetadata({ params }: { params: { postId: string }}) {
+  const posts = getSortedPostsData() //deduped 
+  const { postId } = params
+
+  const post = posts.find(post => post.id === postId)
+
+  if (!post) {
+    return {
+      title: 'Post Not Found'
+    }
+  }
+
+  return {
+    title: post.title,
+  }
+}
+
 export default async function Post({ params }: { params: { postId: string }}) {
   const posts = getSortedPostsData() //deduped 
   const { postId } = params
